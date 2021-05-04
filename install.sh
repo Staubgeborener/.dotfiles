17d2f77ba779efdecd3b2750c3d672e87401b6d9
Staubgeborener <mail@staubgeborener.de>
2021-05-04 16:43:54 +0200

    add polybar to install script

--- install.sh
+++ install.sh
@@ -108,6 +108,21 @@ ninja
 sudo ninja install
 cd $dotfilespath
 
+#polybar
+sudo apt-get install \
+  cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev \
+  libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev \
+  libxcb-util0-dev libxcb-xkb-dev pkg-config python3-xcbgen \
+  xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev \
+  libiw-dev libcurl4-openssl-dev libpulse-dev \
+  libxcb-composite0-dev xcb libxcb-ewmh2 libjsoncpp-dev python3-sphinx
+git clone https://github.com/jaagr/polybar.git ~/polybar
+cd ~/polybar
+git tag # see what version do you need
+git checkout 3.4.1
+./build.sh
+cd $dotfilespath
+
 rm ~/.zshrc
 stow -t ~ */
 source ~/.zshrc
