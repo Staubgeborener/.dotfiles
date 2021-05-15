25b4d20c1e476a1f02cfd524070fbfba9b329948
Staubgeborener <mail@staubgeborener.de>
2021-05-15 21:27:48 +0200

    test new i3-gaps installation

--- install.sh
+++ install.sh
@@ -87,14 +87,25 @@ sudo apt install -y \
 sudo apt -t buster-backports install -y polybar
 
 #i3-gaps
-sudo apt install -y dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
-git clone https://github.com/Airblader/i3 ~/i3
-cd ~/i3
-mkdir -p build && cd build
+#sudo apt install -y dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+#git clone https://github.com/Airblader/i3 ~/i3
+#cd ~/i3
+#mkdir -p build && cd build
 #meson --prefix /usr/local
-meson ..
-ninja
+#meson ..
+#ninja
 ##sudo ninja install
+
+mkdir /tmp/build 
+cd /tmp/build
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+git checkout gaps && git pull
+sudo apt install meson asciidoc
+meson -Ddocs=true -Dmans=true ../build
+meson compile -C ../build
+sudo meson install -C ../build
+
 cd $dotfilespath
  
 #cleaning up
