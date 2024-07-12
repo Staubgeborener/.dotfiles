5d6739b386c5639409ac42b8803fc444cf7f1ae0
Staubgeborener <eric.schroeder@protonmail.com>
2024-07-12 09:07:26 +0200

    neovim.sh aktualisieren

--- install-scripts/neovim.sh
+++ install-scripts/neovim.sh
@@ -3,7 +3,7 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-echo "export PATH="$PATH:/opt/nvim-linux64/bin"" > ~/.bashrc
+echo "export PATH="$PATH:/opt/nvim-linux64/bin"" >> ~/.bashrc
 source ~/.bashrc
 sudo rm -r ~/.config/nvim/
 
